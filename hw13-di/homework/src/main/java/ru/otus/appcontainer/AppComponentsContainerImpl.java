f68bc8583bc65902c205ae3665c1f7b819174597
PetrVlS <sidelevpetya@mail.ru>
2022-05-14 17:19:29 +0300

    hw13-comments taken into account

--- hw13-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw13-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -3,10 +3,7 @@ package ru.otus.appcontainer;
 import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
-import ru.otus.config.AppConfig;
 
-import java.lang.reflect.AnnotatedType;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.*;
@@ -16,25 +13,23 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
-    public AppComponentsContainerImpl(Class<?> initialConfigClass) throws InvocationTargetException, IllegalAccessException {
+    public AppComponentsContainerImpl(Class<?> initialConfigClass) throws ReflectiveOperationException {
         processConfig(initialConfigClass);
     }
 
-    private void processConfig(Class<?> configClass) throws InvocationTargetException, IllegalAccessException {
+    private void processConfig(Class<?> configClass) throws ReflectiveOperationException {
         checkConfigClass(configClass);
         var methods = getOrderedMethods(configClass);
+        var constructor = configClass.getDeclaredConstructor();
+        var configClazz = constructor.newInstance();
 
         for (Method method : methods) {
             var annotation = method.getAnnotation(AppComponent.class);
-            Object resultInvokeMethod;
             var parameters = method.getParameters();
-            if (parameters.length != 0) {
-                var args = initializationParameters(parameters);
-                resultInvokeMethod = method.invoke(new AppConfig(), args);
-            } else {
-                resultInvokeMethod = method.invoke(new AppConfig());
-            }
-            fillAppComponents(resultInvokeMethod, annotation, method);
+            var args = initParameters(parameters);
+            var resultInvokeMethod = method.invoke(configClazz, args);
+            appComponents.add(resultInvokeMethod);
+            appComponentsByName.put(annotation.name(), resultInvokeMethod);
         }
     }
 
@@ -46,36 +41,41 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        var clazzName = componentClass.getName();
-        return (C) appComponentsByName.get(clazzName);
+        C component = null;
+        for (Object appComponent : appComponents) {
+            if (componentClass.isAssignableFrom(appComponent.getClass())) {
+                component = (C) appComponent;
+                break;
+            }
+        }
+        checkComponent(component);
+        return component;
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return (C) appComponentsByName.get(componentName);
+        var component = (C) appComponentsByName.get(componentName);
+        checkComponent(component);
+        return component;
     }
 
-    private Object[] initializationParameters(Parameter[] parameters) {
+    private <C> void checkComponent(C component) {
+        if (component == null) {
+            throw new IllegalArgumentException("Component not found");
+        }
+    }
+
+    private Object[] initParameters(Parameter[] parameters) throws ReflectiveOperationException {
         var args = new Object[parameters.length];
         for (int j = 0; j < parameters.length; j++) {
-            args[j] = getAppComponent(parameters[j].getAnnotatedType().toString());
+                var componentClass = Class.forName(parameters[j].getAnnotatedType().toString());
+                var parameter = getAppComponent(componentClass);
+                args[j] = parameter;
         }
         return args;
     }
 
-    private void fillAppComponents(Object resultInvokeMethod, AppComponent annotation, Method method) {
-        appComponentsByName.put(annotation.name(), resultInvokeMethod);
-        appComponentsByName.put(method.getAnnotatedReturnType().toString(), resultInvokeMethod);
-        appComponentsByName.put(resultInvokeMethod.getClass().getName(), resultInvokeMethod);
-        var interfaces = method.getReturnType().getAnnotatedInterfaces();
-        if (interfaces.length > 0) {
-            for (AnnotatedType annotatedType : interfaces) {
-                appComponentsByName.put(annotatedType.toString(), resultInvokeMethod);
-            }
-        }
-    }
-
-    private List<Method> getOrderedMethods(Class<?> configClass){
+    private List<Method> getOrderedMethods(Class<?> configClass) {
         var methods = configClass.getDeclaredMethods();
         return Arrays.stream(methods).sorted(Comparator.comparing(method -> method.getAnnotation(AppComponent.class).order())).toList();
     }
